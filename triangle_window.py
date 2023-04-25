137446d74de91afe7d0e0170e72442c10991ac34
Vedmix <u.vadim11@gmail.com>
2023-04-26 00:05:46 +0300

    Update 0.08 (changing the interface)

--- triangle_window.py
+++ triangle_window.py
@@ -1,21 +1,94 @@
-from tkinter import *
-from functions_triangle import*
-import matplotlib.pyplot as plt
-def show_result():
-    window.withdraw()
-    result_window = Toplevel()
-    result_window.title("Result")
-    result_window.geometry("550x500+700+500")
-    result_window.configure(bg='#09070F')
-    text1 = entry1.get()
-    text2 = entry2.get()
-    text3 = entry3.get()
+
+import customtkinter
+
+from math import*
+
+from math import acos, degrees, sqrt, sin
+
+def side(point_x_1, point_y_1, point_x_2, point_y_2):
+    side = sqrt(((point_x_1 - point_x_2) ** 2) + ((point_y_1 - point_y_2) ** 2))
+    return side
+
+def perimeter_t(side1, side2, side3):
+    return float((side1+side2+side3))
+
+def perimeter_e(side1, side2, side3,side4):
+    return float((side1+side2+side3+side4))
+
+def square(side1, side2, side3):
+    p = perimeter_t(side1, side2, side3) / 2
+    return (sqrt(p * (p - side1) * (p - side2) * (p - side3)))
+
+def lenght_median(side1, side2, side3):  # side3 вычитаем
+    lenght_median = sqrt(2 * side1 ** 2 + 2 * side2 ** 2 - side3 ** 2) / 2
+    return lenght_median
+
+
+def angle(side1, side2, side3):  # side3 вычитаем
+    angle = (acos((side1 ** 2 + side2 ** 2 - side3 ** 2) / (2.0 * side1 * side2)))
+    return angle
+
+
+def equation_side(point_x_1, point_y_1, point_x_2, point_y_2):
+    A = float(point_y_2 - point_y_1)
+    B = float(point_x_1 - point_x_2)
+    C = float(point_x_2 * point_y_1 - point_x_1 * point_y_2)
+    return '(' + str(A) + ')' + 'x + ' + '(' + str(B) + ')' + 'y + ' + '(' + str(C) + ')' + ' = 0'
+
+
+def line_coefficients(p1, p2):
+    A = (p1[1] - p2[1])
+    B = (p2[0] - p1[0])
+    C = (p1[0] * p2[1] - p2[0] * p1[1])
+    return A, B, -C
+
+
+def intersection_x(L1, L2):
+    D = L1[0] * L2[1] - L1[1] * L2[0]
+    Dx = L1[2] * L2[1] - L1[1] * L2[2]
+    if D != 0:
+        x = Dx / D
+        return x
+
+
+def intersection_y(L1, L2):
+    D = L1[0] * L2[1] - L1[1] * L2[0]
+    Dy = L1[0] * L2[2] - L1[2] * L2[0]
+    if D != 0:
+        y = Dy / D
+        return y
+
+customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+app = customtkinter.CTk()
+app.geometry("290x230+700+500")
+app.title("Triangle")
+app.resizable(width=False, height=False)
+
+
+def result():
+
+    app.withdraw()
+
+    result =customtkinter.CTk()
+    result.title("Triangle")
+    result.geometry("400x280")
+
+    frame_1 = customtkinter.CTkFrame(master=result)
+    frame_1.pack(pady=20, padx=20, fill="both", expand=True)
+
+    text1 = entry_1.get()
+    text2 = entry_2.get()
+    text3 = entry_3.get()
+
     splitted_text1 = text1.split(';')
     x1, y1 = [float(splitted_text1[0]), float(splitted_text1[1])]
     splitted_text2 = text2.split(';')
     x2, y2 = [float(splitted_text2[0]), float(splitted_text2[1])]
     splitted_text3 = text3.split(';')
     x3, y3 = [float(splitted_text3[0]), float(splitted_text3[1])]
+
     AB = side(x1, y1, x2, y2)
     BC = side(x2, y2, x3, y3)
     AC = side(x1, y1, x3, y3)
@@ -25,107 +98,78 @@ def show_result():
     angle_BCA = angle(AB, BC, AC)
 
     if (x1 == x2 == x3) or (y1 == y2 == y3):
-        label1 = Label(result_window, text="Такого треугольника не существует!", font=("Arial", 14))
-        label1.pack()
+
+        result.geometry("500x190")
+        app.resizable(width=False, height=False)
+        label_1 = customtkinter.CTkLabel(master=frame_1, text="Такого треугольника не существует!", justify=customtkinter.LEFT,font=("Arial", 15))
+        label_1.pack(pady=10, padx=10)
+        button_1 = customtkinter.CTkButton(master=frame_1, width=100, text='Закрыть', font=("Arial", 15),command=result.quit)
+        button_1.pack(pady=10, padx=10)
+
     if (AB + BC > AC) and (BC + AC > AB) and (AB + AC > BC):
-        label1 = Label(result_window, bg='#09070F',fg='white', text="Введённые координаты:", font=("Arial", 14))
-        label1.place(x=20, y=20)
-        label2 = Label(result_window, bg='#09070F',fg='white', text="Точка А: ({},{})".format(x1, y1), font=("Arial", 14))
-        label2.place(x=20, y=50)
-        label3 = Label(result_window, bg='#09070F',fg='white', text="Точка В: ({},{})".format(x2, y2), font=("Arial", 14))
-        label3.place(x=20, y=80)
-        label4 = Label(result_window, bg='#09070F',fg='white', text="Точка С: ({},{})".format(x3, y3), font=("Arial", 14))
-        label4.place(x=20, y=110)
-        label5 = Label(result_window, bg='#09070F',fg='white', text="Длины сторон:", font=("Arial", 14))
-        label5.place(x=20, y=140)
-        label6 = Label(result_window, bg='#09070F',fg='white', text="Сторона АВ: {}".format('%.2f' % AB), font=("Arial", 14))
-        label6.place(x=20, y=170)
-        label7 = Label(result_window, bg='#09070F',fg='white', text="Сторона ВС: {}".format('%.2f' % BC), font=("Arial", 14))
-        label7.place(x=20, y=200)
-        label8 = Label(result_window, bg='#09070F', fg='white',text="Сторона АC: {}".format('%.2f' % AC), font=("Arial", 14))
-        label8.place(x=20, y=230)
 
-        label9=Label(result_window, bg='#09070F', fg='white',text="Углы треуголька:", font=("Arial", 14))
-        label9.place(x=260, y=20)
-        label6 = Label(result_window, bg='#09070F',fg='white', text="Угол BAC: {}°".format('%.2f' % degrees(angle_BAC)), font=("Arial", 14))
-        label6.place(x=260, y=50)
-        label7 = Label(result_window, bg='#09070F', fg='white',text="Угол ABC: {}°".format('%.2f' % degrees(angle_ABC)), font=("Arial", 14))
-        label7.place(x=260, y=80)
-        label8 = Label(result_window, bg='#09070F', fg='white',text="Угол BCA: {}°".format('%.2f' % degrees(angle_BCA)), font=("Arial", 14))
-        label8.place(x=260, y=110)
+        if (AB + BC > AC) and (BC + AC > AB) and (AB + AC > BC):
 
-        label13 = Label(result_window, bg='#09070F',fg='white', text="Уравнения сторон:", font=("Arial", 14))
-        label13.place(x=260, y=140)
-        label14 = Label(result_window, bg='#09070F', fg='white',text="АВ: {}".format(equation_side(x1, y1, x2, y2)), font=("Arial", 14))
-        label14.place(x=260, y=170)
-        label15 = Label(result_window, bg='#09070F',fg='white', text="ВС: {}".format(equation_side(x2, y2, x3, y3)), font=("Arial", 14))
-        label15.place(x=260, y=200)
-        label16 = Label(result_window, bg='#09070F',fg='white', text="АC: {}".format(equation_side(x1, y1, x3, y3)), font=("Arial", 14))
-        label16.place(x=260, y=230)
+            if AB == BC == AC:
+                type_triangle='Равносторонний треугольник'
 
-        if AB == BC == AC:
-            label17 = Label(result_window, bg='#09070F',fg='white', text="{}".format('Тип >> "Равносторонний треугольник"'), font=("Arial", 14))
-            label17.place(x=100, y=260)
+            elif (round(angle_BAC,15) == round((pi/2),15)) or (round(angle_ABC,15) == round((pi/2),15)) or (round(angle_BCA,15) == round((pi/2),15)):
+                type_triangle='Прямоугольный треугольник'
 
-        elif (angle_BAC == 1, 5708) or (angle_ABC == 1, 5708) or (angle_BCA == 1, 5708):
-            label17 = Label(result_window, bg='#09070F',fg='white', text="{}".format('Тип >> "Прямоугольный треугольник"'), font=("Arial", 14))
-            label17.place(x=100, y=260)
+            elif (round(angle_BAC,15) < round((pi/2),15)) and (round(angle_ABC,15) < round((pi/2),15)) and (round(angle_BCA,15) < round((pi/2),15)):
+                type_triangle='Остроугольный треугольник'
 
-        elif (angle_BAC < 1, 5708) and (angle_ABC < 1, 5708) and (angle_BCA < 1, 5708):
-            label17 = Label(result_window, bg='#09070F',fg='white', text="{}".format('Тип >> "Остроугольный треугольник"'), font=("Arial", 14))
-            label17.place(x=100, y=260)
+            elif (round(angle_BAC,15) > round((pi/2),15)) or (round(angle_ABC,15) > round((pi/2),15)) or (round(angle_BCA,15) > round((pi/2),15)):
+                type_triangle='Тупоугольный треугольник'
 
-        elif (angle_BAC > 1, 5708) or (angle_ABC > 1, 5708) or (angle_BCA > 1, 5708):
-            label17 = Label(result_window, bg='#09070F',fg='white', text="{}".format('Тип >> "Тупоугольный треугольник"'), font=("Arial", 14))
-            label17.place(x=100, y=260)
+            elif (AB != BC == AC) or (AB == BC != AC) or (AB == AC != BC):
+                type_triangle='Равнобедренный треугольник'
 
-        elif (AB != BC == AC) or (AB == BC != AC) or (AB == AC != BC):
-            label17 = Label(result_window, bg='#09070F',fg='white', text="{}".format('Тип >> Равнобедренный треугольник'), font=("Arial", 14))
-            label17.place(x=100, y=260)
-        label17 = Label(result_window, bg='#09070F',fg='white', text="Периметр треугольника: {}".format('%.2f' % perimeter_t(AB, BC, AC)),font=("Arial", 14))
-        label17.place(x=100, y=290)
-        label17 = Label(result_window, bg='#09070F',fg='white', text="Площадь треугольника: {}".format('%.2f' % square(AB, BC, AC)),font=("Arial", 14))
-        label17.place(x=100, y=320)
+        text_1 = customtkinter.CTkTextbox(master=frame_1, width=320, height=170)
+        text_1.place(x=20,y=20)
+        text_1.insert("0.0", "Введённые координаты:\nTочкa A: ({}, {})\nTочкa B: ({}, {})\nTочкa C: ({}, {})\n"
+                             "Тип: {}\n"
+                             "Периметр треугольника: {}\n"
+                             "Площадь треугольника: {}\n"
+                             "Длины сторон:\nСторона АВ: {}\nСторона ВС: {}\nСторона АC: {}\n"
+                             "Углы треуголька:\nУгол BAC: {}°\nУгол ABC: {}°\nУгол BCA: {}°\n"
+                             "Уравнения сторон:\nАВ: {}\nВС: {}\nАC: {}\n"
+        .format(x1, y1, x2, y2, x3, y3,type_triangle,'%.2f' % perimeter_t(AB, BC, AC),'%.2f' % square(AB, BC, AC),
+        '%.2f' % AB,'%.2f' % BC,'%.2f' % AC,'%.2f' % degrees(angle_BAC),'%.2f' % degrees(angle_ABC),
+        '%.2f' % degrees(angle_BCA),equation_side(x1, y1, x2, y2),equation_side(x2, y2, x3, y3),equation_side(x1, y1, x3, y3)))
+        button_1 = customtkinter.CTkButton(master=frame_1, width=100, text='Закрыть', font=("Arial", 15),
+                                           command=app.quit)
+        button_1.place(x=140, y=200)
 
-        fig, ax = plt.subplots()
-
-        x, y = [x1, x2, x3], [y1, y2, y3]
-        x1, y1 = [x1, x3], [y1, y3]
-
-        plt.plot(x, y, color='black')
-        plt.plot(x1, y1, color='black')
-        plt.plot(x, y, 'o')
-        plt.plot(x1, y1, 'o')
-
-        ax.grid()
-        plt.title('Ваш треугольник:')
-        plt.show()
 
     else:
-        label1 = Label(result_window, text="Такого треугольника не существует!", font=("Arial", 14))
-        label1.pack()
-    result_window.mainloop()
+        result.geometry("500x150")
+        app.resizable(width=False, height=False)
 
+        label_1 = customtkinter.CTkLabel(master=frame_1, text="Такого треугольника не существует!",justify=customtkinter.LEFT, font=("Arial", 15))
+        label_1.pack(pady=10, padx=10)
 
-window = Tk()
-window.title('triangle')
-window.geometry("420x200+700+500")
-window.configure(bg='#09070F')
-window.resizable(width=False, height=False)
-label1 = Label(text="Координаты точки А:", bg='#192133', font=("Arial", 14))
-label1.place(x=20, y=20)
-entry1 = Entry(window, width=15, bg='#192133', font=('Arial', 14))
-entry1.place(x=220, y=20)
-label2 = Label(text="Координаты точки B:", bg='#192133', font=("Arial", 14))
-label2.place(x=20, y=60)
-entry2 = Entry(window, width=15, bg='#192133', font=('Arial', 14))
-entry2.place(x=220, y=60)
-label3 = Label(text="Координаты точки C:", bg='#192133', font=("Arial", 14))
-label3.place(x=20, y=100)
-entry3 = Entry(window, width=15, bg='#192133', font=('Arial', 14))
-entry3.place(x=220, y=100)
-btm_done = Button(window, bg='#192133', text='Готово', font=('Arial', 14), command=show_result)
-btm_done.place(x=180, y=150, width=100, height=30)
-btm_escape = Button(window, bg='#192133', text='Закрыть', font=('Arial', 14), command=window.quit)
-btm_escape.place(x=290, y=150, width=100, height=30)
-window.mainloop()
+        button_1 = customtkinter.CTkButton(master=frame_1, width=100, text='Закрыть', font=("Arial", 15),command=result.quit)
+        button_1.pack(pady=10, padx=10)
+
+    result.mainloop()
+
+frame_1 = customtkinter.CTkFrame(master=app)
+frame_1.pack(pady=20, padx=20, fill="both", expand=True)
+
+entry_1 = customtkinter.CTkEntry(master=frame_1, width=210, placeholder_text="       Координаты точки А", font=("Arial", 15))
+entry_1.place(x=20,y=20)
+
+entry_2 = customtkinter.CTkEntry(master=frame_1, width=210, placeholder_text="       Координаты точки B", font=("Arial", 15))
+entry_2.place(x=20,y=60)
+
+entry_3 = customtkinter.CTkEntry(master=frame_1, width=210, placeholder_text="       Координаты точки C", font=("Arial", 15))
+entry_3.place(x=20,y=100)
+
+button_1 = customtkinter.CTkButton(master=frame_1,width=100, text='Готово', font=("Arial", 15), command=result)
+button_1.place(x=20,y=140)
+
+button_1 = customtkinter.CTkButton(master=frame_1, width=100, text='Закрыть', font=("Arial", 15),command=app.quit)
+button_1.place(x=129, y=140)
+
+app.mainloop()
